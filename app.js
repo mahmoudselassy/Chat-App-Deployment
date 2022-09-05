f0df82639a23ae64f401bbaf423de7e6c3586eb8
mahmoudselassy <59484122+mahmoudselassy@users.noreply.github.com>
2022-09-05 17:54:36 +0200

    Refactor

--- app.js
+++ app.js
@@ -1,56 +1,97 @@
 const body = document.querySelector("body");
 const nameText = document.querySelector("#name-text");
+const roomText = document.querySelector("#room-text");
+const roomName = document.querySelector("#room-name");
 let HOST = location.origin.replace(/^http/, "ws");
 let ws = new WebSocket(HOST);
 let client = null;
 let currentRoomId = null;
 ws.onmessage = (message) => {
     let res = JSON.parse(message.data);
-    if (res.title === "createClient") {
-        client = res.data;
-        body.innerHTML = `<h1>Welcome </h1>
-        <label for="room-text">Enter Room Id :</label>
-        <input id="room-text" type="text" />
-        <button id="join-btn">Join Room</button>
-        <br />
-        <button style="margin-top: 20px" id="create-btn">create Room</button>`;
-    } else if (res.title === "createRoom") {
-        body.innerHTML += `<h3 style="color:green;">Room Created Successfully!</h3>
-                        <h4>Room Id : ${res.data.roomId}</h4>`;
+    if (res.title === "createRoom") {
+        roomText.value = res.data.roomId;
     } else if (res.title === "joinRoom") {
+        client = {
+            clientId: res.data.clientId,
+            clientName: res.data.clientName,
+        };
+        currentRoomId = res.data.roomId;
+
         body.innerHTML = `
-          <ul>
-    </ul>
-    <input id="message-text" placeholder="message">
-    <button id="send">Send</button>`;
-    } else if (res.title === "sendMessage") {
+            <div class="chat-container">
+            <header class="chat-header">
+                <h1>Chatter</h1>
+                <a href="index.html" class="btn">Leave Room</a>
+            </header>
+            <main class="chat-main">
+                <div class="chat-sidebar">
+                    <h3><i class="fas fa-comments"></i> Room Name:</h3>
+                    <h2 id="room-name">${res.data.roomName}</h2>
+                    <h3><i class="fas fa-users"></i> Users</h3>
+                    <ul id="users">
+                    </ul>
+                </div>
+                <div class="chat-messages">
+                </div>
+            </main>
+            <div class="chat-form-container">
+                <form id="chat-form">
+                    <input
+                        id="message-text"
+                        type="text"
+                        placeholder="Enter Message"
+                        required
+                    />
+                    <button type="button" id="send" class="btn">
+                        <i class="fas fa-paper-plane"></i> Send
+                    </button>
+                </form>
+            </div>
+        </div>
+`;
+        for (let memberName of res.data.membersNames) {
+            const el = document.createElement("li");
+            el.innerHTML = memberName;
+            document.querySelector("#users").append(el);
+        }
+    } else if (res.title === "memberAdded") {
         const el = document.createElement("li");
-        el.innerHTML = res.message;
-        document.querySelector("ul").appendChild(el);
+        el.innerHTML = res.memberName;
+        document.querySelector("#users").append(el);
+    } else if (res.title === "sendMessage") {
+        const messages = document.querySelector(".chat-messages");
+        messages.innerHTML += `<div class="message">
+                        <p class="meta">${
+                            client.clientId === res.clientId
+                                ? "me"
+                                : res.senderName
+                        } <span>${new Date(Date.now()).toLocaleString("en-US", {
+            hour: "numeric",
+            minute: "numeric",
+            hour12: true,
+        })}</span></p>
+                        <p class="text">
+                            ${res.clientMessage}
+                        </p>
+                    </div>`;
     }
 };
 body.addEventListener("click", (e) => {
     let clickedElement = e.target;
-    if (clickedElement.id === "name-btn") {
-        ws.send(
-            JSON.stringify({
-                title: "createClient",
-                name: nameText.value,
-            })
-        );
-    } else if (clickedElement.id === "create-btn") {
+    if (clickedElement.id === "create-btn") {
+        if (!roomName.value) return alert("enter room name for creating!");
         ws.send(
             JSON.stringify({
                 title: "createRoom",
+                roomName: roomName.value,
             })
         );
     } else if (clickedElement.id === "join-btn") {
-        currentRoomId = document.querySelector("#room-text").value;
         ws.send(
             JSON.stringify({
                 title: "joinRoom",
-                clientId: client.clientId,
-                roomId: currentRoomId,
+                roomId: document.querySelector("#room-text").value,
+                name: nameText.value,
             })
         );
     } else if (clickedElement.id === "send") {
@@ -60,6 +101,7 @@ body.addEventListener("click", (e) => {
             JSON.stringify({
                 title: "sendMessage",
                 clientName: client.clientName,
+                senderId: client.clientId,
                 roomId: currentRoomId,
                 message,
             })
--- server.js
+++ server.js
@@ -21,22 +21,13 @@ const createId = () => {
 };
 WebSocket.on("request", (request) => {
     let connection = request.accept(null, request.origin);
-    let client = { connection };
+    let clientId = createId();
+    let client = { connection, clientId };
     connection.on("message", async (message) => {
         let res = JSON.parse(message.utf8Data);
-        if (res.title === "createClient") {
-            let clientId = createId();
-            client.name = res.name;
-            clients.set(clientId, client);
-            connection.send(
-                JSON.stringify({
-                    title: "createClient",
-                    data: { clientId, clientName: client.name },
-                })
-            );
-        } else if (res.title === "createRoom") {
+        if (res.title === "createRoom") {
             let roomId = createId();
-            rooms.set(roomId, new Array());
+            rooms.set(roomId, { name: res.roomName, members: new Array() });
             connection.send(
                 JSON.stringify({
                     title: "createRoom",
@@ -45,24 +36,49 @@ WebSocket.on("request", (request) => {
             );
         } else if (res.title === "joinRoom") {
             const roomId = res.roomId;
-            const clientId = res.clientId;
-            let newArr = rooms.get(roomId);
-            newArr.push(clientId);
-            rooms.set(roomId, newArr);
+            client.name = res.name;
+            clients.set(clientId, client);
+            let members = rooms.get(roomId).members;
+            let roomName = rooms.get(roomId).name;
+            members.push(clientId);
+            let membersNames = [];
+            for (let memberId of members) {
+                membersNames.push(clients.get(memberId).name);
+            }
+            rooms.set(roomId, { name: roomName, members });
             connection.send(
                 JSON.stringify({
                     title: "joinRoom",
+                    data: {
+                        clientId,
+                        clientName: client.name,
+                        roomId,
+                        roomName,
+                        membersNames,
+                    },
                 })
             );
+            const clientsInRoom = rooms.get(roomId).members;
+            for (let clientId of clientsInRoom) {
+                if (clientId !== client.clientId)
+                    clients.get(clientId).connection.send(
+                        JSON.stringify({
+                            title: "memberAdded",
+                            memberName: membersNames.at(-1),
+                        })
+                    );
+            }
         } else if (res.title === "sendMessage") {
             const senderName = res.clientName;
             const clientMessage = res.message;
-            const clientsInRoom = rooms.get(`${res.roomId}`);
-            for (let clientId of clientsInRoom) {
-                clients.get(clientId).connection.send(
+            const clientsInRoom = rooms.get(res.roomId).members;
+            for (let id of clientsInRoom) {
+                clients.get(id).connection.send(
                     JSON.stringify({
                         title: "sendMessage",
-                        message: `${senderName}: ${clientMessage}`,
+                        clientId: res.senderId,
+                        senderName: senderName,
+                        clientMessage: clientMessage,
                     })
                 );
             }
@@ -71,7 +87,6 @@ WebSocket.on("request", (request) => {
             rooms = new Map();
             connection = null;
             client = null;
-            console.log(clients, rooms);
         }
     });
 });
